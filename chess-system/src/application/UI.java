cb736445eea9dd5b0df139a1c83255f6dfd44460
Matheus Monteiro <monteiromatheus@id.uff.br>
2020-06-17 15:29:43 -0300

    Moving Pieces

--- chess-system/src/application/UI.java
+++ chess-system/src/application/UI.java
@@ -6,7 +6,10 @@
 package application;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -34,6 +37,20 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+
+        } catch (RuntimeException e) {
+
+            throw new InputMismatchException("Error reading ChessPositon: Valid values are from a1 to h8");
+        }
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
 
         for (int i = 0; i < pieces.length; i++) {
@@ -50,17 +67,15 @@ public class UI {
 
     public static void printPiece(ChessPiece piece) {
 
-      if (piece == null) {
+        if (piece == null) {
             System.out.print("-");
-        }
-        else {
+        } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
-	}
+    }
 }
